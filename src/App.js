2601767531ed96acea1211b7ae75b373118c2abc
legion <tsvetan@airheadventures.com>
2022-04-22 13:14:38 +0300

    routing fixes

--- src/App.js
+++ src/App.js
@@ -9,10 +9,8 @@ import { ProtectedRoute } from "./helpers/protectedRoute";
 import Navigation from "./components/navbar/index";
 
 function App() {
-  const routeComponents = routesConfig.map(({ path, component }, i) => (
-    <React.Fragment key={i}>
-      <ProtectedRoute exact path={path} component={component} />
-    </React.Fragment>
+  const routeComponents = routesConfig.map(({ path, component }) => (
+    <ProtectedRoute exact path={path} component={component} />
   ));
 
   return (
